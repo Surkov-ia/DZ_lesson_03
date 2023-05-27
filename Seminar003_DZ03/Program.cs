e9e68ce0e7da8505464d88e3128ce4abd00fcab9
Gocha <Frost220v@gmail.com>
2023-05-27 14:40:18 +0500

    программа расчитывает и выдает таблицу кубов числа N

---
+++ Seminar003_DZ03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число = ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count=1;
+while(num>=count)
+{
+    Console.WriteLine($"N = {Math.Pow(count,3) }");
+    count=count+1;
+}
