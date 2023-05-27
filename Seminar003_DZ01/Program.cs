ff81ef24ad6570eec323e3cbd94693700394f7ef
Gocha <Frost220v@gmail.com>
2023-05-27 13:38:30 +0500

    програмный код решает зеркальное число мы ввели или нет

---
+++ Seminar003_DZ01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string txt = Convert.ToString(num);
+int size = txt.ToString().Length;
+
+if(size==5)
+{
+    if (txt[0]==txt[4]&&txt[1]==txt[3])
+    {
+        Console.WriteLine($"Число является зеркальным{txt}");
+    }
+    else
+    {
+        Console.WriteLine($"Число не является зеркальным{txt}");
+    }
+}
+else
+{
+    Console.WriteLine($"Неверное значение{txt}");
+}
